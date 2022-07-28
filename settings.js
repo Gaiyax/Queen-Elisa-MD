bef786fe77404ff13afd1389464a5771017d1115
Gaiyax <103402620+Gaiyax@users.noreply.github.com>
2022-07-28 09:36:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,19 +32,19 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
-global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
-global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
-global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
+global.owner = ['94770154460'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.pemilik = ['94770154460'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['94770154460'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.pengguna = 'Gaveesha Randith' // Your name ඔබේ නම 
+global.botnma = '𝙂𝘼𝙑𝙀𝙀 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
 global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
-global.packname = 'Queen Elisa Sticker' // Sticker package name 
-global.author = 'Dark Maker' // Sticker Autor name 
+global.packname = 'Gaiya bro' // Sticker package name 
+global.author = '𝙂𝘼𝙄𝙔𝘼' // Sticker Autor name 
 global.sessionName = 'session'
 global.prefa = ['#','!','/',''] 
 global.sp = '🔵'
-// Alive massage , ඔබේ බොගේ alive massage  එක මෙහි ඇති ` ` මෙම ලකුනු ඇතුලේ ඔබට අවශ්‍ය ඔනිම දෙයක් Type කරන්න.💃
-global.alivelogo = `https://telegra.ph/file/1a2dee71baa975091620d.jpg`
+// Alive massage , ඔබේ බොගේ alive massage  එක මෙහි ඇති ` 𝙃𝙄 𝙂𝘼𝙄𝙔𝘼 𝙞𝙨 𝘼𝙇𝙄𝙑𝙀 ❤️ ` මෙම ලකුනු ඇතුලේ ඔබට අවශ්‍ය ඔනිම දෙයක් Type කරන්න.💃
+global.alivelogo = `https://i.ibb.co/rwZQ7qS/Picsart-22-07-27-08-34-49-506.jpg`
 global.alive =`Hello i am alive now
 
 Download song type .yt 
